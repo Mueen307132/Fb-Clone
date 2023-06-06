aea173569da5f7a888f5aff5119c92cba59194cf
Moeen Ul Hasan <moeen.hassan@techverx.com>
2023-06-06 16:58:35 +0500

    Added auth for ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
      protect_from_forgery with: :exception
+     skip_before_action :verify_authenticity_token
      before_action :authenticate_user!
 
      before_action :configure_permitted_parameters, if: :devise_controller?
@@ -9,4 +10,6 @@ class ApplicationController < ActionController::Base
                devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :avatar)}
                devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email,:avatar, :password, :password_confirmation, :current_password)}
           end
+
+          
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,11 +5,58 @@ import jquery from "jquery";
 window.jQuery = jquery;
 window.$ = jquery;
 
-$(document).ready(function () {
-  console.log("I AM OK");
+$(function () {
   $(".edit-button-class").on("click", function () {
     var id = $(this).data("post-id");
     $(`#edit_id_${id}`).show();
     $(`#post_${id}`).hide();
+  });
+
+  $(".comments-button").on("click", async function () {
+    var id = $(this).data("post-id");
+
+    $(`#comments_post_${id}`).toggle();
+  });
+  $(".like").on("click", function (e) {
+    var id = $(this).data("post-id");
+    debugger;
+    $.ajax({
+      url: `/posts/${id}/likes`,
+      type: "POST",
+      data: { post_id: id },
+      success: function () {
+        $(`#like_${id}`).hide();
+        $(`#unlike_${id}`).show();
+      },
+    });
+  });
+  $(".unlike").on("click", async function (e) {
+    var post_id = $(this).data("post-id");
+    var id = $(this).data("like-id");
+    debugger;
+    $.ajax({
+      url: `/posts/${post_id}/likes/${id}`,
+      type: "DELETE",
+
+      data: { post_id: post_id, id: id },
+      success: function () {
+        $(`#like_${id}`).show();
+        $(`#unlike_${id}`).hide();
+      },
+    });
+  });
+  $(".delete-post").on("click", async function (e) {
+    var id = $(this).data("post-id");
+    console.log(id);
+    debugger;
+    $.ajax({
+      url: `/posts/${id}`,
+      type: "DELETE",
+      data: { id: id },
+      success: function () {
+        console.log("HI");
+        $(`#post_${id}`).hide();
+      },
+    });
   });
 });
